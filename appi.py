8f0951ac855b953bb020e7d32d669bdad945e3ed
sammaash <38855300+sammaash@users.noreply.github.com>
2024-05-03 22:10:27 +0300

    Update appi.py

--- appi.py
+++ appi.py
@@ -1,6 +1,6 @@
-import streamlit as st
-import pandas as pd
-import joblib
+streamlit as st
+pandas as pd
+joblib
 
 # Load the trained decision tree model
 model = joblib.load('decision_tree_model.pkl')
